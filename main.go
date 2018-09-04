9bdfa2af40a34ddc399884923af1884b8a0c8772
Nico Ulbricht <nico.ulbricht@live.de>
2018-09-04 19:03:16 +0200

    restructure scene

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"math"
-	"math/rand"
 
 	"github.com/wahtye/gotracer/geometry"
 	"github.com/wahtye/gotracer/material"
@@ -16,18 +15,27 @@ func main() {
 
 func buildScene(size float64) *render.Scene {
 	scene := render.NewScene()
-	scene.AddObject(material.NewDiffuseMaterial(1.), geometry.NewPlane(geometry.NewVector(0, 0, 110), geometry.NewVector(0, 0, -1.)))
-	scene.AddObject(material.NewDiffuseMaterial(1.), geometry.NewPlane(geometry.NewVector(0, 0, -110), geometry.NewVector(0, 0, 1.)))
-	scene.AddObject(material.NewEmissiveMaterial(1., 5000), geometry.NewPlane(geometry.NewVector(size+1, 0, 0), geometry.NewVector(-1., 0, 0)))
+	diffuseMaterial := material.NewDiffuseMaterial(1.)
+	emissiveMaterial := material.NewEmissiveMaterial(1., 5000)
+	floorY := .5*size/2 + size/2
 
-	sphereCount := 6
+	scene.AddObject(diffuseMaterial, geometry.NewPlane(geometry.NewVector(0, 0, .5*size), geometry.NewVector(0, 0, -1.)))
+	scene.AddObject(diffuseMaterial, geometry.NewPlane(geometry.NewVector(0, 0, -5*size), geometry.NewVector(0, 0, 1.)))
+
+	scene.AddObject(emissiveMaterial, geometry.NewPlane(geometry.NewVector(.75*size/2+size/2, 0, 0), geometry.NewVector(-1., 0, 0)))
+	scene.AddObject(diffuseMaterial, geometry.NewPlane(geometry.NewVector(-.75*size/2+size/2, 0, 0), geometry.NewVector(1., 0, 0)))
+
+	scene.AddObject(diffuseMaterial, geometry.NewPlane(geometry.NewVector(0, -.75*size/2+size/2, 0), geometry.NewVector(0, 1., 0)))
+	scene.AddObject(diffuseMaterial, geometry.NewPlane(geometry.NewVector(0, floorY, 0), geometry.NewVector(0, -1., 0)))
+
+	sphereCount := 3
+	radius := size / 16.
 	for i := 0; i < sphereCount; i++ {
-		angle := float64(2.*math.Pi/float64(sphereCount)) * float64(i)
-		spherePosition := geometry.NewVector(math.Sin(angle)*size/4.+size/2., math.Cos(angle)*size/4.+size/2., 80)
-		sphere := geometry.NewSphere(spherePosition, size/16.)
+		angle := float64(1.8*math.Pi/float64(sphereCount)) * float64(i)
+		spherePosition := geometry.NewVector(math.Sin(angle)*size/6.+size/2., floorY-radius, math.Cos(angle)*size/6.-size/4.)
+		sphere := geometry.NewSphere(spherePosition, radius)
 
-		mat := material.NewDiffuseMaterial(rand.Float64()*.5 + .5)
-		scene.AddObject(mat, sphere)
+		scene.AddObject(diffuseMaterial, sphere)
 	}
 
 	return scene
