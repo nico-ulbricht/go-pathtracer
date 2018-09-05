00fb2fc03246cc1808a1a3283c57590450de28c8
Nico Ulbricht <nico.ulbricht@live.de>
2018-09-05 12:26:52 +0200

    remove normal flipping since we trust in our normals 🙌

--- material/diffuse.go
+++ material/diffuse.go
@@ -13,10 +13,6 @@ func NewDiffuseMaterial(reflectance float64) *DiffuseMaterial {
 }
 
 func (mat *DiffuseMaterial) Reflect(ray *geometry.Ray, intersection *geometry.Intersection) *geometry.Ray {
-	if ray.Direction.Dot(intersection.Normal) > 0. {
-		intersection.Normal = intersection.Normal.MultiplyScalar(-1.)
-	}
-
 	hemisphereVector := geometry.NewHemisphereVector()
 	ray.Direction = hemisphereVector.RotateTowards(intersection.Normal)
 	ray.Origin = intersection.Point
--- material/reflective.go
+++ material/reflective.go
@@ -15,11 +15,6 @@ func NewReflectiveMaterial(reflectance, roughness float64) *ReflectiveMaterial {
 
 func (mat *ReflectiveMaterial) Reflect(ray *geometry.Ray, intersection *geometry.Intersection) *geometry.Ray {
 	directionDotNormal := ray.Direction.Dot(intersection.Normal)
-	if directionDotNormal > 0. {
-		intersection.Normal = intersection.Normal.MultiplyScalar(-1.)
-	}
-
-	directionDotNormal = ray.Direction.Dot(intersection.Normal)
 	ray.Direction = ray.Direction.Subtract(intersection.Normal.MultiplyScalar(2. * directionDotNormal))
 	ray.Origin = intersection.Point
 	ray.Intensity *= mat.reflectance * .96
